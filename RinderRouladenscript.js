41de24c3398351d6bbc995c51b561c3483f88804
Mert-Ince-lab <mertince2322@gmail.com>
2025-10-20 23:46:56 +0200

    js fixt

--- RinderRouladenscript.js
+++ RinderRouladenscript.js
@@ -13,7 +13,7 @@ function ammount() {
     document.getElementById('Scheiben Frühstücksspeck').innerHTML = (3 * numberofpersons.value) + 'Scheiben Frühstücksspeck'
     document.getElementById('Butterschmalz').innerHTML = (0.5 * numberofpersons.value) + 'Butterschmalz'
     document.getElementById('Knollensllerie').innerHTML = (0.25 * numberofpersons.value) + 'Stück(e) Knollensllerie'
-    document.getElementById('Möhren').innerHTML = (0.25 * numberofpersons.value) + 'Stück(e) Knollensllerie'
+    document.getElementById('Möhren').innerHTML = (0.25 * numberofpersons.value) + 'Möhren'
     document.getElementById('Stangen').innerHTML = (0.13 * numberofpersons.value) + 'Stange(n) Lauch'
     document.getElementById('Rotwein').innerHTML = (0.13 * numberofpersons.value) + 'ml Rotwein'
     document.getElementById('Rinderfond').innerHTML = (0.13 * numberofpersons.value) + 'Rinderfond'
